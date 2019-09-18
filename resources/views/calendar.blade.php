e94ed39869cf279d1660dc4e9a9df5bc0cb70356
Robert <rsztabralowski@gmail.com>
2019-09-18 23:45:09 +0100

    Added layout to calendar blade

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -1,16 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Calendar</title>
-</head>
-<body>
-    @auth
-        <h1>You are logged in</h1>
-    @else 
-        <h1>You are a guest</h1>
-    @endauth
-</body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        @auth
+            <h1>You are logged in</h1>
+        @else 
+            <h1>You are a guest</h1>
+        @endauth
+    </div>  
+@endsection  
