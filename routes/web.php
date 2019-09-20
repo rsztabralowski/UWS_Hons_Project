005693381bbe744a254c3e9a38aee545c7887372
Robert <rsztabralowski@gmail.com>
2019-09-20 02:11:17 +0100

    Added routes for resources

--- routes/web.php
+++ routes/web.php
@@ -19,3 +19,7 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/calendar', 'CalendarController@index')->name('calendar');
+
+Route::resource('booking', 'BookingController');
+Route::resource('room', 'RoomController');
+Route::resource('customer', 'CustomerController');
