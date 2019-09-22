cf4decd71c4768b19e3970bee90c66836a1f580d
Robert <rsztabralowski@gmail.com>
2019-09-22 03:02:37 +0100

    Added view blades for admin  bookings

--- resources/views/admin/bookings/index.blade.php
+++ resources/views/admin/bookings/index.blade.php
@@ -6,7 +6,7 @@
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                 <div class="panel-heading btn-primary">
-                    WELCOME TO ADMIN PANEL
+                    WELCOME TO BOOKINGS SECTION
                 </div>
             </div>
         </div>
