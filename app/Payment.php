23bf79fe725c5ab2dab52a3c547061a13d34280f
Robert <rsztabralowski@gmail.com>
2019-09-24 22:22:30 +0100

    Updated Payment model

--- app/Payment.php
+++ app/Payment.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Payment extends Model
 {
-    //
+    protected $fillable = ['amount', 'paypaltoken', 'booking_id', 'customer_id'];
+
+
+    public function booking()
+    {
+        return $this->belongsTo(Booking::class, 'booking_id');
+    }
+
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class, 'customer_id');
+    }
 }
