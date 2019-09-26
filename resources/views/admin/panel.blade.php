d752228d9b36bc43fa1fbe56269031c8b1a8cf4b
Robert <rsztabralowski@gmail.com>
2019-09-26 01:00:36 +0100

    Working on panel blade layout and style

--- resources/views/admin/panel.blade.php
+++ resources/views/admin/panel.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
+    {{-- <div class="row"> --}}
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
                     {{-- {{$bookings}} --}}
@@ -30,6 +30,6 @@
                 </div>
             </div>
         </div>
-    </div>
+    {{-- </div> --}}
 </div>
 @endsection
