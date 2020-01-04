b307470dcfae4b3aadf2e83ac40360559865479f
Robert <rsztabralowski@gmail.com>
2020-01-04 02:10:23 +0000

    Added noscript tag to admin panel

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,6 +1,13 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <noscript>
+        <div style="text-align: center; background-color: red; color:white; padding:40px">
+            <h1>JavaScript is turned off or your browser does not support it</h1>
+            <br>
+            <h4>This site will not work properly</h4>
+        </div>
+    </noscript>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
